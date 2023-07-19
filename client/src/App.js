0bd3ee5bce04439d25f29b7fe8b9580d495fd86a
carrottechplus <blohzzang@gmail.com>
2023-07-19 11:23:15 +0900

    리액트,서버간의 데이터 전달

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function App() {
-	return <h1>Hello2</h1>;
+	const item = { name: 'John' };
+
+	useEffect(() => {
+		axios
+			.post('/api/send', item)
+			.then((res) => {
+				// 서버쪽에서 응답이 성공적으로 넘어오면 ㅐㅎ당 값을 콘솖문으로 출력
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	return <h1>Hello3</h1>;
 }
 
 export default App;
