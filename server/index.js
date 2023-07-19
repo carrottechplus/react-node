e5d988c0941a8b9ac0f294c22cc32d921b3fa05c
carrottechplus <blohzzang@gmail.com>
2023-07-19 10:21:36 +0900

    init

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5500;
+
+app.get('/', (req, res) => {
+	res.send('Hello World3');
+});
+
+app.listen(port, () => {
+	console.log(`Server app listening port ${port}`);
+});
