ca13e54c56d223d83b5591cbdc57edcd4d1f9f42
carrottechplus <blohzzang@gmail.com>
2023-07-19 10:46:57 +0900

    리액트에서 빌드된 파일 노드 서버를 통해서 구동

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 5500;
 
+// express에서 client측 build 폴더까지의 경로를 static하게 지정
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.get('/', (req, res) => {
-	res.send('Hello World3');
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.get('*', (req, res) => {
+	// 이상한 url로 들어올경우 처리? e.g. 404 error
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(port, () => {
