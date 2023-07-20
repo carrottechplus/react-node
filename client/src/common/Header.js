2198915357e6c771c2f5939608da2981a20d54be
carrottechplus <blohzzang@gmail.com>
2023-07-20 11:12:33 +0900

    Styled component로 header 스타일링

--- client/src/common/Header.js
+++ client/src/common/Header.js
@@ -1,14 +1,39 @@
 import { Link, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Header() {
-	const activeStyle = { color: 'aqua' };
+	const activeStyle = { color: 'orange' };
+	const HeaderWrap = styled.header`
+		width: 350px;
+		height: 100vh;
+		background: #222;
+		position: fixed;
+		top: 0;
+		left: 0;
+		padding: 50px;
+	`;
+	const Logo = styled.h1`
+		margin-bottom: 40px;
+		a {
+			font: 40px/1 'arial';
+			color: #fff;
+		}
+	`;
+	const Gnb = styled.ul`
+		a {
+			display: block;
+			padding: 10px;
+			font: bold 16px/1 'arial';
+			color: #bbb;
+		}
+	`;
 
 	return (
-		<header>
-			<h1>
+		<HeaderWrap>
+			<Logo>
 				<Link to='/'>LOGO</Link>
-			</h1>
-			<ul id='gnb'>
+			</Logo>
+			<Gnb>
 				<li>
 					<NavLink to='/list' style={(props) => (props.isActive ? activeStyle : null)}>
 						Show List
@@ -19,8 +44,8 @@ function Header() {
 						Write Post
 					</NavLink>
 				</li>
-			</ul>
-		</header>
+			</Gnb>
+		</HeaderWrap>
 	);
 }
 
