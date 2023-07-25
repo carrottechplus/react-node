f4b3112171ac76baecc61fb8a0f5f2138ba06e0b
carrottechplus <blohzzang@gmail.com>
2023-07-25 11:02:33 +0900

    redux-toolkit 설치 및 userSlice 작업

---
+++ client/src/redux/userSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+//reducer만들기
+
+const userSlice = createSlice({
+	name: 'user',
+	initialState: {
+		//담고싶은 값 지정하기
+		displayName: '',
+		uid: '',
+	},
+	reducers: {
+		loginUser: (state, action) => {
+			state.displayName = action.payload.displayName;
+			state.uid = action.payload.uid;
+		},
+		logoutUser: (state) => {
+			state.displayName = '';
+			state.uid = '';
+		},
+	},
+});
+
+export const { loginUser, logoutUser } = userSlice.actions;
+export default userSlice.reducer;
